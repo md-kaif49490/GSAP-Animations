bb4b84e78e87fb32b96749458f0559d5cff52f90
Kaif <kaif49490@gmail.com>
2025-07-14 23:08:03 +0530

    Playing With Box

---
+++ 02_PlayingWithBox/script.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+const anime = gsap.to('.card', {
+    opacity: 1,
+    rotation:360,
+    background:'#ff6f51',
+    scale: 1.2,
+    duration: 2,
+    ease: 'bounce',
+    repeat: -1,
+    yoyo: true,
+    repeatDelay: 1,
+    //paused: true,
+    stagger:1,
+    borderRadius: '50%',
+
+
+});
+
